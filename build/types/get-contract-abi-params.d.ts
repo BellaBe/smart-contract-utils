109fa877665a02501176387467a841bdc5b4ee67
bellabe <bella.belgarokova@hotmail.com>
2023-03-07 17:13:28 +0000

    remove build from repo

--- build/types/get-contract-abi-params.d.ts
+++
@@ -1,5 +0,0 @@
-export interface GetContractABIParams {
-    explorer_base_url: string;
-    explorer_api_key: string;
-    contract_address: string;
-}
--- build/utils/get-contract-abi.d.ts
+++
@@ -1,2 +0,0 @@
-import { GetContractABIParams } from "src/types/get-contract-abi-params";
-export declare const getContractABI: (params: GetContractABIParams) => Promise<any>;
